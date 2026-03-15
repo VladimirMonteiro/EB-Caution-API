fe97d5a3d2ec9ac7d952717430942ef34e09c9f5
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2026-03-15 20:29:01 -0300

    initial commit

---
+++ src/main/java/com/outercode/caution/CautionApplication.java
@@ -0,0 +1,13 @@
+package com.outercode.caution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CautionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CautionApplication.class, args);
+	}
+
+}

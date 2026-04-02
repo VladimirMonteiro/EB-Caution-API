570507f61db43c4352849b0374994e29c87a33f3
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2026-04-02 16:52:09 -0300

    feat: added user domain

---
+++ src/main/java/com/outercode/caution/entities/User.java
@@ -0,0 +1,38 @@
+package com.outercode.caution.entities;
+
+import com.outercode.caution.entities.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String warName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}

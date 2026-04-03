2e5545200d9e52a661618c67f061e4f543716cd1
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2026-04-03 14:51:11 -0300

    feat: validate token

--- src/main/java/com/outercode/caution/infra/security/TokenService.java
+++ src/main/java/com/outercode/caution/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.outercode.caution.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.outercode.caution.entities.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,20 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Error while generating token", exception);
+        }
+    }
+
+    public String validateToken (String token) {
+        try {
+
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
         }
     }
 

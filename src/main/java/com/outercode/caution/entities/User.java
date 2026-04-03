ee1c8c95719b3b35ec2a1f46732c8c237718057c
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2026-04-03 15:08:25 -0300

    fix: added enumerated annotation in role

--- src/main/java/com/outercode/caution/entities/User.java
+++ src/main/java/com/outercode/caution/entities/User.java
@@ -36,6 +36,7 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
 

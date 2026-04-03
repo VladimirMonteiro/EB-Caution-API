caf9d60954800e08cbcc0c44bb77f32aff8b30b6
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2026-04-03 11:08:57 -0300

    fix: resolve error of invalid name

--- src/main/java/com/outercode/caution/repositories/CautionItemRepository.java
+++ src/main/java/com/outercode/caution/repositories/CautionItemRepository.java
@@ -1,7 +1,8 @@
 package com.outercode.caution.repositories;
 
+import com.outercode.caution.entities.CautionItem;
 import com.outercode.caution.entities.pk.CautionItemPk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CautionItemRepository extends JpaRepository<CautionItemRepository, CautionItemPk> {
+public interface CautionItemRepository extends JpaRepository<CautionItem, CautionItemPk> {
 }

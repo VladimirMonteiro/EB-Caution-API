09bd3163642dd887103fb8c29c03bb5d16f2e69c
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2026-04-03 14:43:59 -0300

    feat: generate token

---
+++ src/main/java/com/outercode/caution/infra/security/TokenService.java
@@ -0,0 +1,37 @@
+package com.outercode.caution.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.outercode.caution.entities.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken (User user) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while generating token", exception);
+        }
+    }
+
+    private Instant generateExpirationDate () {
+        return LocalDateTime.now().plusHours(1000).toInstant(ZoneOffset.of("-03:00"));
+    }
+}

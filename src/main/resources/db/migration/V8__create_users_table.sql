fe95a22b36e8947663e2c2b51fc4e77592e2fcc8
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2026-04-13 07:46:05 -0300

    feat: added column grad and active

--- src/main/resources/db/migration/V8__create_users_table.sql
+++ src/main/resources/db/migration/V8__create_users_table.sql
@@ -1,7 +1,9 @@
-create table users (
+ create table users (
+        active boolean not null,
         created_at timestamp(6) not null,
         id uuid not null,
         email varchar(255) not null unique,
+        grad varchar(255) not null,
         password varchar(255) not null,
         role varchar(255) not null check (role in ('ARMORER','SUB_ARMORER')),
         war_name varchar(255) not null,

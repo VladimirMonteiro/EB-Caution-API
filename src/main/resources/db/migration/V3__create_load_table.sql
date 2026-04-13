229c12b8ffa3ec57b4c0063cc963e5da619535f3
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2026-04-13 07:45:42 -0300

    feat: added column cia

--- src/main/resources/db/migration/V3__create_load_table.sql
+++ src/main/resources/db/migration/V3__create_load_table.sql
@@ -2,7 +2,7 @@ create table load (
         active boolean not null,
         created_at timestamp(6) not null,
         id uuid not null,
-        user_id uuid not null,
+        cia varchar(255) not null,
         pel_name varchar(255) not null,
         primary key (id)
 );
